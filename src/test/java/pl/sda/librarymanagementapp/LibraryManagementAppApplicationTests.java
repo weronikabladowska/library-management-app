c188ea793633963b9b49127c4232a0f9e94bfb0c
Weronika Bladowska <weronika.maciejka@gmail.com>
2020-12-19 12:18:10 +0100

    Initial commit

---
+++ src/test/java/pl/sda/librarymanagementapp/LibraryManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.librarymanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
