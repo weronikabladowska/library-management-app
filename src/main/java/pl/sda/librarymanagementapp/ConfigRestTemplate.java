bc7ae9a0689e7a9e989c00a0076c0e70925023cd
Weronika Bladowska <weronika.maciejka@gmail.com>
2021-01-04 22:08:04 +0100

    BookService commit

---
+++ src/main/java/pl/sda/librarymanagementapp/ConfigRestTemplate.java
@@ -0,0 +1,13 @@
+package pl.sda.librarymanagementapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ConfigRestTemplate {
+
+@Bean
+    public RestTemplate confgurationRestTemplate(){return new RestTemplate();}
+
+}
